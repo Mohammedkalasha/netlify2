e297e49b547306385e6eaf9d6a204ba922a6cd80
Mohammedkalasha <109412459+Mohammedkalasha@users.noreply.github.com>
2022-09-01 15:17:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function foo(){
+    var 
+     res=document.getElementById("main").value;
+    console.log(res);
+
+    res = document.getElementById("main1").value;
+    console.log(res);
+
+    res = document.getElementById("main2").value;
+    console.log(res);
+
+    res = document.getElementById("main3").value;
+    console.log(res);
+}
